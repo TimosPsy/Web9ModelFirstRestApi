2d53d3532cd284d5da1e823a670ad7877f1458be
Timos <psychoson91@gmail.com>
2026-05-12 18:48:56 +0300

    Implement layered architecture with auth, DI, and logging

--- SchoolApp/DTO/UserLoginDTO.cs
+++ SchoolApp/DTO/UserLoginDTO.cs
@@ -14,6 +14,6 @@ namespace SchoolApp.DTO
             "one digit, and one special character")]
         public string? Password { get; set; }
 
-        public bool KeepLoggedIn { get; set; }
+       // public bool KeepLoggedIn { get; set; }
     }
 }
